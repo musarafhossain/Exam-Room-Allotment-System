0882cbfd94f35d0741c28706cc32f12a41021fbd
Musaraf Hossain <146651402+musarafhossain@users.noreply.github.com>
2026-05-11 03:05:43 +0530

    make setting value optional

--- backend/src/validators/Setting.Validator.ts
+++ backend/src/validators/Setting.Validator.ts
@@ -3,7 +3,7 @@ import { z } from "zod";
 class SettingValidator {
     updateSettingSchema = z.object({
         key: z.string().min(1, "Key is required"),
-        value: z.string().min(1, "Value is required")
+        value: z.string()
     });
 }
 
